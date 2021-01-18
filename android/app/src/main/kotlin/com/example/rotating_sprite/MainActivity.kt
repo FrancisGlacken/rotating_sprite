15885ac8d8fa29d21855e1123f1c07253ac3cf01
Francis <francisglacken@gmail.com>
2021-01-18 02:52:43 -0500

    First commit

---
+++ android/app/src/main/kotlin/com/example/rotating_sprite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rotating_sprite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+import 'package:flame/anchor.dart';
+import 'package:flame/game.dart';
+import 'package:flame/components/sprite_component.dart';
+import 'package:flame/extensions/vector2.dart';
+import 'package:flutter/material.dart' hide Image;
+import 'package:flame/game/base_game.dart';
+
+Vector2 spriteSize = Vector2.all(50);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final game = ExampleGame();
+  runApp(GameWidget(game: game));
+}
+
+class ExampleGame extends BaseGame {
+  SpriteComponent planetComponent; 
+
+  @override
+  Future<void> onLoad() async {
+    final Image planetImage = await images.load('planet.png');
+    final Vector2 spriteSize = Vector2(100, 100);
+
+    // Create a SpriteComponent with size and image, then center the component's position and anchor
+    planetComponent = SpriteComponent.fromImage(spriteSize, planetImage)
+    ..x = size.x/2
+    ..y = size.y/2
+    ..anchor = Anchor.center;
+
+    // Add the component to game
+    add(planetComponent);
+  }
+
+  @override
+    void update(double t) {
+      // TODO: implement update
+      super.update(t);
+
+      planetComponent.angle = planetComponent.angle + t; 
+    } 
+
+}
